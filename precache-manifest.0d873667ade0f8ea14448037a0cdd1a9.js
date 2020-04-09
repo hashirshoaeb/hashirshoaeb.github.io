42f444b5bad980f9bf1902e7c22590c34e79b12f
hashirshoaeb <hashirshoaeb@gmail.com>
2020-04-09 17:10:03 +0500

    Updates

---
+++ precache-manifest.0d873667ade0f8ea14448037a0cdd1a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a9d1bd14fbfba404f76e1e7cd77c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1cb9559fb2f2df3b8e8a",
+    "url": "/static/css/main.1f7ed24e.chunk.css"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/static/js/2.73412ab1.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/static/js/2.73412ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb9559fb2f2df3b8e8a",
+    "url": "/static/js/main.5455e61b.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/static/media/resume.0c246fa0.pdf"
+  }
+]);
